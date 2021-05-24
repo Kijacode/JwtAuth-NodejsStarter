093e10baad132198a53e8e18febda0233d988f85
Kijacode <kijadanford@gmail.com>
2021-05-24 06:26:04 +0300

    feat:db url

--- db/database.js
+++ db/database.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 
 console.log('db is here');
 var con = mongoose.connect(
-	"mongodb+srv://Kijacode:7864drftgh4536cxvds33@auth-hqhzc.mongodb.net/test?retryWrites=true&w=majority",
+	"mongodb+srv://admin:admin7@cluster0.abyky.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
 	{ userNewUrlParser: true },
 	() => console.log('connect to db')
 ).then( async () =>{
